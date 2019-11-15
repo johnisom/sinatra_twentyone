a7a5116bdf98454e0f0df34c1ee03822237a357c
John Isom <John.Isom2001@gmail.com>
2019-11-15 13:40:40 -0700

    Add more styling

--- twenty-one.rb
+++ twenty-one.rb
@@ -4,7 +4,17 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'tilt/erubis'
 
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+def flash(message, type=:neutral)
+  session[:flash] = { message: message, type: type }
+end
+
 get '/' do
+  flash('Hi there!', :success)
   erb :index
 end
 
