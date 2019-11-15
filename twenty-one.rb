1abe9a381495eb1e15271d991bb9ad0753d83714
John Isom <John.Isom2001@gmail.com>
2019-11-15 15:56:33 -0700

    Add play page

--- twenty-one.rb
+++ twenty-one.rb
@@ -9,6 +9,27 @@ configure do
   set :session_secret, 'secret'
 end
 
+helpers do
+  def each_dealer_card
+    [%w[5 S], %w[J H], %w[A C], %w[2 D], %w[K H]].each do |value, suite|
+      yield value, suite
+    end
+  end
+
+  def each_player_card
+    [%w[5 S], %w[J H], %w[A C], %w[2 D], %w[K H], %w[K S]].each do |value, suite|
+      yield value, suite
+    end
+  end
+end
+
+before '/play' do
+  @computer_hand_value = 21
+  @human_hand_value = 21
+  @computer_score = 3
+  @human_score = 13
+end
+
 def flash(message, type=:neutral)
   session[:flash] = { message: message, type: type }
 end
